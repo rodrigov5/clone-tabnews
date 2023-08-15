53dae5ed23d3fd48dba275f5d1cc6c9f7fe2075b
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2023-08-15 01:06:58 +0000

    adiciona nova tela ao site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import React from 'react';
+
 function Home() {
-    return <h1>Início</h1>
+    const pageStyle = {
+        backgroundColor: '#2D2D2A',
+        textAlign: 'center',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        fontFamily: 'Roboto, sans-serif', 
+        color: 'white',                  
+    };
+
+    const textStyles = {
+        fontSize: '24px', 
+        marginBottom: '20px', 
+    };
+
+    return (
+        <div style={pageStyle}>
+            <p style={textStyles}>João, eu te amo. Se você me ama, dá uma risadinha.</p>
+        </div>
+    );
 }
 
 export default Home;
