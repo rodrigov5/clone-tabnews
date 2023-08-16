f7e7e1b1d4442368b449b384237df37dbea23dba
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2023-08-16 14:28:33 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone da plataforma tabnews do curso.dev - Com objetivo de criar um projeto real do inicio ao fim.
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    const pageStyle = {
-        backgroundColor: '#2D2D2A',
-        textAlign: 'center',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        fontFamily: 'Roboto, sans-serif', 
-        color: 'white',                  
-    };
+  const pageStyle = {
+    backgroundColor: "#2D2D2A",
+    textAlign: "center",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "white",
+  };
 
-    const textStyles = {
-        fontSize: '24px', 
-        marginBottom: '20px', 
-    };
+  const textStyles = {
+    fontSize: "24px",
+    marginBottom: "20px",
+  };
 
-    return (
-        <div style={pageStyle}>
-            <p style={textStyles}>João, eu te amo. Se você me ama, dá uma risadinha.</p>
-        </div>
-    );
+  return (
+    <div style={pageStyle}>
+      <p style={textStyles}>Teste</p>
+    </div>
+  );
+}
+
+function Teste() {
+  return <h1>OI</h1>;
 }
 
 export default Home;
